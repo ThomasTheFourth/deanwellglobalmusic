5309d96b217eedb27edbf0c75a4e8b33fcaaf1f4
Thomas Pinkney Barnwell IV <thomas@krakenlabs.io>
2021-02-05 00:27:35 -0500

    wip

--- src/pages/Releases/index.js
+++ src/pages/Releases/index.js
@@ -1,27 +1,51 @@
 import {
-  Card,
+	Card,
+	CardContent,
 	makeStyles,
+	Typography,
 } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
 	container: {
-    maxWidth: 1200,
-    margin: '0 auto',
+		maxWidth: 1200,
+		display: 'flex',
+		flexWrap: 'wrap',
+		alignItems: 'center',
+		justifyContent: 'center',
+		margin: '0 auto',
 	},
-  card: {
-    margin: 16,
-    width: 350,
-    float: 'left',
-  }
+	card: {
+		margin: 16,
+		width: 350,
+		textAlign: 'left',
+	},
+	image: {
+		width: '100%',
+		marginBottom: 8,
+	}
 }));
 
 export default function Releases() {
-	const { card, container } = useStyles();
-  return (
-    <div className={ container }>
-      <Card className={ card }>test</Card>
-      <Card className={ card }>test</Card>
-      <Card className={ card }>test</Card>
-    </div>
-  )
+	const { card, container, image, imageContainer } = useStyles();
+	console.log(process);
+	return (
+		<div className={ container }>
+			<Card className={ card }>
+				<CardContent>
+					<img className={ image } src={process.env.PUBLIC_URL + '/images/across_the_depths.jpg'} />
+					<Typography color="textSecondary">
+						Picture One
+					</Typography>
+					<Typography variant="h6" component="h2">
+						Across The Depths of Seven Lakes
+					</Typography>
+					<Typography variant="body2" component="p">
+						Catalog Number: DWGM-20
+						<br />
+						Formats: Vinyl LP/Cassette Tape/Digital
+					</Typography>
+				</CardContent>
+			</Card>
+		</div>
+	)
 };
